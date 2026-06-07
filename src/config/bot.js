bcf7f4adf43dbf34d51c457ce977fc31042c7d43
arand606 <yfathan089@gmail.com>
2026-06-07 19:24:30 +0700

    Change bot activity type from Playing to Watching

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Team M00NKID Join Today!",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
