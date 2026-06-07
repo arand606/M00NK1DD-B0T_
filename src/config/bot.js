5d42876b226828a21b80334375ce2e883bf6c885
arand606 <yfathan089@gmail.com>
2026-06-07 20:56:51 +0700

    Update bot activity name to include 'Watching You'

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Team M00NK1DD Join Today!",
+        name: "Watching You | Team M00NK1DD Join Today!",
         // Activity type number (0 = Playing).
         type: 3, 
       },
