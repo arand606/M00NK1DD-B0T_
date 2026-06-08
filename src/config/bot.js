1df521311fbd818c59b5226d098ca838bf183d50
arand606 <yfathan089@gmail.com>
2026-06-08 19:58:46 +0700

    Change bot activity type from 4 to 3

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Watching Channels | Team M00NK1DD Join Today!",
         // Activity type number (0 = Playing).
-        type: 4, 
+        type: 3, 
       },
     ],
   },
