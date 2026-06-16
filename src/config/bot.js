404a5644dbfe5f92f25bb308bb88681628f41e34
arand606 <yfathan089@gmail.com>
2026-06-16 15:33:28 +0700

    Change cycleInterval from 13000 to 12000 ms

--- src/config/bot.js
+++ src/config/bot.js
@@ -15,7 +15,7 @@ export const botConfig = {
     status: "online",
 
     // How often to switch statuses in milliseconds (30000 = 30 seconds)
-    cycleInterval: 13000,
+    cycleInterval: 12000,
 
     // Activity lines shown under the bot name.
     // `type` number mapping from Discord:
